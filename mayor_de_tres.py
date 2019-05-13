b4b23ee4c93f11cecb4d975bd2525516488e7ae4
Matias <matixroyalgaming@gmail.com>
2019-05-13 05:58:36 -0400

    Añadiendo combate pokemon

---
+++ mayor_de_tres.py
@@ -0,0 +1,7 @@
+numero_inicial = 10
+
+while numero_inicial > 0:
+        numero_inicial -= 1
+        print (numero_inicial + 1)
+
+print("He terminado")
---
+++ test_input.py
@@ -0,0 +1,2 @@
+numero_del_usuario = input("Dime un numero: ")
+print(numero_del_usuario)
